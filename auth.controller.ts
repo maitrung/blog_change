7c627adbdfaef2ed06b403b470a34f6368157aff
MAI_TRUNG <86087059+maitrung@users.noreply.github.com>
2022-07-05 17:34:35 +0700

    Add files via upload

--- auth.controller.ts
+++ auth.controller.ts
@@ -6,16 +6,17 @@ import { CreateUser } from './dto/createuser.dto';
 export class AuthController {
     constructor(private readonly userService:AuthService){}
     @Post('/login')
-    public async login(@Res() res,@Body()username, password ){
-        const getUser = await this.userService.FindOne(username,password);
-        if(!getUser) throw new NotFoundException('Login Faid');
-        return res.status(HttpStatus.OK).json(getUser);
+    public async login(@Res() res, @Body()username):Promise<any>{
+        const getUser = await this.userService.FindOne(username);
+        if(getUser==="1") throw new NotFoundException('sai mật khẩu hoặc tên đăng nhập');
+        return res.status(HttpStatus.OK).json("login thành công");
+             
     }
     @Post('/register')
     public async Register(@Res()res,@Body() createUser:CreateUser){
         const registerUser = await this.userService.Register(createUser);
-        if(!registerUser) throw new NotFoundException('Register Faid');
-        return res.status(HttpStatus.OK).json(registerUser);
+        if(registerUser==="1") throw new NotFoundException('Tên đăng nhập đã được sử dụng');
+        return res.status(HttpStatus.OK).json(createUser);
     }
 
 }
